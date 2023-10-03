b59566ca2dd9129a8bcab88a03ef294afcbb417e
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-03 05:24:50 -0300

    add cadastro completo

--- vitalab/usuarios/views.py
+++ vitalab/usuarios/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-  return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:    
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'Sua senha deve ter 7 ou mais digitos')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            # TODO: validar se o username do usuário não existe
+            # Username deve ser único!
+            user = User.objects.create_user(
+                first_name=primeiro_nome,
+                last_name=ultimo_nome,
+                username=username,
+                email=email,
+                password=senha,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema, contate um admnistrador.')
+            return redirect('/usuarios/cadastro')
+
+        return redirect('/usuarios/cadastro')
+    
+
