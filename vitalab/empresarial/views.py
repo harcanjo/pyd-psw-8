d1d262b6c403a8592b0b8def89c7d72a3484db0c
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-06 14:11:43 -0300

    add gerar senha to pdf

--- vitalab/empresarial/views.py
+++ vitalab/empresarial/views.py
@@ -5,6 +5,7 @@ from django.db.models import Value
 from django.db.models.functions import Concat
 from django.contrib.admin.views.decorators import staff_member_required
 from exames.models import SolicitacaoExame
+from .utils import gerar_pdf_exames, gerar_senha_aleatoria
 
 
 @staff_member_required
@@ -43,3 +44,17 @@ def proxy_pdf(request, exame_id):
 
     response = exame.resultado.open()
     return FileResponse(response)
+
+
+@staff_member_required
+def gerar_senha(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    if exame.senha:
+        # Baixar o documento da senha já existente
+        return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
+
+    senha = gerar_senha_aleatoria(9)
+    exame.senha = senha
+    exame.save()
+    return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
